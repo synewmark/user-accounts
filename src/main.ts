c6348b54148b24245eea4ce2859fee6bb5a511cb
synewmark <76981619+synewmark@users.noreply.github.com>
2022-07-26 02:20:31 -0400

    Now throws exception on bad/missing input in requests
    Uses ValidationPipes

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
@@ -12,7 +13,8 @@ async function bootstrap() {
     },
   });
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   await redis.listen();
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
 }
 bootstrap();
--- src/users.entity.ts
+++ src/users.entity.ts
@@ -1,16 +1,20 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class Users {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty()
   @Column()
   name: string;
-
+  
+  @IsEmail()
   @Column()
   email: string;
-
+  
+  @IsNotEmpty()
   @Column()
   country: string;
 }
