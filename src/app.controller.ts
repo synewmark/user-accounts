dcc1e34a21542df9b1f9827cd7c04e537635174c
synewmark <76981619+synewmark@users.noreply.github.com>
2022-07-26 02:21:27 -0400

    Minor changes on comments and variable names

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ConflictException, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, ConflictException, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Users } from 'src/users.entity';
 
@@ -7,9 +7,9 @@ import { Users } from 'src/users.entity';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('getUser/:id')
-  async getUser(@Param('id') id): Promise<Users> {
-    let result: Promise<Users> = this.appService.getUser(id);
+  @Get('getUser/:name')
+  async getUser(@Param('name') name): Promise<Users> {
+    let result: Promise<Users> = this.appService.getUser(name);
     // not sure best way to throw excception here - look into interceptors?
     if (!await result) {
       throw new NotFoundException("User with given name not found");
@@ -18,7 +18,7 @@ export class AppController {
   }
 
   @Post('addUser')
-  async addUser(@Body() body : Users) : Promise<string> {
-    return this.appService.addUser(body);
+  async addUser(@Body() user : Users) : Promise<string> {
+    return this.appService.addUser(user);
   }
 }
